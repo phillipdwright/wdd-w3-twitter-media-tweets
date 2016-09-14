e4be0cecea833bc6a12d70c4cad6d3e73414dc64
Phil <wright.phillip@gmail.com>
2016-09-14 01:41:16 +0000

    Extend models

--- twitter_media_tweets/twitter/models.py
+++ twitter_media_tweets/twitter/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
+
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
@@ -13,11 +17,33 @@ class Tweet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
     content = models.CharField(max_length=140, blank=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
-
+    
+    content_type = models.ForeignKey(ContentType, null=True)
+    object_id = models.IntegerField(null=True)
+    media = GenericForeignKey('content_type', 'object_id')
+    
+    @property
+    def type(self):
+        if str(self.content_type) == 'image tweet':
+            return 'image'
+        elif str(self.content_type) == 'video tweet':
+            return 'video'
+        return 'text'
+    
+    class Meta:
+        ordering = ['-created']
 
 class ImageTweet(models.Model):
     image_url = models.URLField()
+    tweet = GenericRelation(Tweet)
+    
+    def __str__(self):
+        return 'image'
 
 
 class VideoTweet(models.Model):
     video_url = models.URLField()
+    tweet = GenericRelation(Tweet)
+    
+    def __str__(self):
+        return 'video'
